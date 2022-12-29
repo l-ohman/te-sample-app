6b0aa514742f92f6c263562daa3f8148e1f70cd5
jake lohman <jakelohman7@gmail.com>
2022-12-29 18:51:19 +0000

    Cleaned up package/directories

--- server/db/db.js
+++
@@ -1,4 +0,0 @@
-const Sequelize = require('sequelize');
-const db = new Sequelize("postgres://localhost:5432/placeholder", { logging: false });
-
-module.exports = db;
--- tests/example.spec.js
+++
@@ -1,7 +0,0 @@
-const { expect } = require("chai");
-
-describe("Tests run successfully", () => {
-    it("Placeholder", () => {
-        expect(1).to.equal(1);
-    })
-});
