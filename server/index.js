a8ecba6ba7023373bbafb30008bd852e156c7e09
l-ohman <jakelohman7@gmail.com>
2023-01-13 10:51:14 -0600

    Removed server and cleaned up package.json

--- server/index.js
+++
@@ -1,8 +0,0 @@
-const port = process.env.PORT || 3000;
-const app = require('./app');
-
-const init = async () => {
-    app.listen(port, ()=> console.log(`Listening on port ${port}`));
-};
-
-init();
